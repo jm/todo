36d1d4110d5fdaaeec6da0f8f8d1656216ef31f4
Jeremy McAnally <jeremymcanally@gmail.com>
2010-04-08 00:28:43 -0500

    Remove problematic new_rails_defaults.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, {
-  :key => '_todo_session',
+  :key => '_todo_session'
 }
 
 # Use the database for sessions instead of the cookie-based default,
